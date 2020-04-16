ae870053278c216de65730c0fd9fd197654b0d04
Ashish Kumar <galgotia.ashish@gmail.com>
2020-04-17 03:23:09 +0530

    Update reader.js

--- js/reader.js
+++ js/reader.js
@@ -2,18 +2,21 @@ var counter = 0;
 var Reader = (function(){
   return {
     start : function(FeedUrl,MaxCount){
+      var x2js = new X2JS();
       var YQLstr = 'SELECT channel.item FROM feednormalizer WHERE output="rss_2.0" AND url ="' + FeedUrl + '" LIMIT ' + MaxCount;
       $.ajax({
-        url: "https://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent(YQLstr) + "&format=json&diagnostics=false&callback=?",
-        dataType: "json",
+        url: "https://news.yahoo.com/rss/mostviewed",
+      //  dataType: "json",
         success : function(response){
-          var feedResponse = response.query.results.rss;
+          var res = x2js.xml_str2json(response);
+          console.log(res);
+          var feedResponse = res.rss;
           for (var i=0 ; i < feedResponse.length ; i++){
             var feed = feedResponse[i].channel.item;
             var articleNode = '<div class="col-md-6"><div class="article-wrapper"><a href="#" class="btn btn-default hide-btn">hide this article</a><a href="'+ feed.link +'" class="article_heading">' + feed.title +  '</a><div class="description">'+ feed.encoded +'</div><p class="about_creator"><span class="creator_name pull-left">'+ feed.creator +'</span><span class="published_on pull-right">'+ feed.pubDate +'</span></p></div></div>';
             $('#feedWrapper').append(articleNode);
           }
-          Reader.correctYoutubeLink();
+        //  Reader.correctYoutubeLink();
           Reader.hideArticle();
         }
       });
